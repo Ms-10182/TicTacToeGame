7ba5f60a29c2d1a7d72d61be98ee1b3b8e912275
Mayank Sharma <22bcs10182@cuchd.in>
2023-12-21 17:54:34 +0530

    winner checking
    add the code of for winner checking loc 26-37.
    added function to enable all the buttons when false is pressed.

--- script.js
+++ script.js
@@ -10,13 +10,31 @@ let winCondition =[
     [0,4,8],
     [2,4,6],
 ];
+
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
         if (buttons[i].innerText !== "X" && buttons[i].innerText !== "O") {
             buttons[i].innerText = mode;
             mode = (mode === "X") ? "O" : "X";
         }
-    });
+        setTimeout(()=>{
+            checkWinning();
+        },0);
+    }); 
+}
+
+const checkWinning=()=>{
+    for(pattern of winCondition){
+        if(buttons[pattern[0]].innerText=="X" || buttons[pattern[0]].innerText=="O"){   //checking if button is not compared when it is empty
+            if(buttons[pattern[0]].innerText==buttons[pattern[1]].innerText &&
+                buttons[pattern[1]].innerText==buttons[pattern[2]].innerText){          //checking if 3 same symbol are in line or not
+                    for(a of buttons){
+                        a.disabled = true;                                              // when winner found disabling all the buttons
+                    }
+                    alert(`${buttons[pattern[1]].innerText} is the winner`);
+            }
+        }
+    }
 }
 
 let resetButton = document.querySelector("#reset");
@@ -24,5 +42,7 @@ let resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click",()=>{
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].innerText = "";
+        buttons[i].disabled=false;
     }
 });
+
