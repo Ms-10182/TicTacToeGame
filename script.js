631465c491df91c952fd029ea1e939f0a9e3275f
Mayank Sharma <22bcs10182@cuchd.in>
2023-12-21 13:36:34 +0530

    added class to game buttons
    instead of making object of button element , made object of class of button so that it dont interfare with the reset button

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let mode = "X";
-let buttons = document.querySelectorAll("button");
-
+let buttons = document.querySelectorAll(".gameButton");
+let winCondition =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
         if (buttons[i].innerText !== "X" && buttons[i].innerText !== "O") {
